4b076e91d3874f120b09eb0a81e1348953b764a6
tbui <nhattuan.dkc@gmail.com>
2022-04-18 17:26:21 +0700

    install dateformat

--- src/component/StaffListComponent.js
+++ src/component/StaffListComponent.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Card, CardBody, CardTitle, CardImg, CardText } from 'reactstrap';
-// import dateFormat from "dateformat";
+import dateFormat from "dateformat";
 
 class StaffList extends Component {
 
@@ -24,12 +24,12 @@ class StaffList extends Component {
                         <CardImg width="100%" src={staff.image} alt={staff.name} />
                         <CardBody>
                             <CardTitle>Họ và tên: {staff.name}</CardTitle>
-                            {/* <CardText>
+                            <CardText>
                                 Ngày sinh: {dateFormat(staff.doB, "dd/mm/yyyy")}
                             </CardText>
                             <CardText>
                                 Ngày vào công ty: {dateFormat(staff.startDate, "dd/mm/yyyy")}
-                            </CardText> */}
+                            </CardText>
                             <CardText>Phòng ban: {staff.department.name}</CardText>
                             <CardText>Số ngày nghỉ còn lại: {staff.annualLeave}</CardText>
                             <CardText>Số ngày đã làm thêm: {staff.overTime}</CardText>
