3692ee82ead805aed15bce499b60018e94854134
tbui <nhattuan.dkc@gmail.com>
2022-04-18 16:36:53 +0700

    render StaffList demo

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
+import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import StaffList from './component/StaffListComponent';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/">Ứng dụng quản lí nhân sự v1.0</NavbarBrand>
-        </div>
-      </Navbar>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ứng dụng quản lí nhân sự v1.0</NavbarBrand>
+          </div>
+        </Navbar>
+        <StaffList />
+      </div>
+    );
+    }
+  
 }
 
 export default App;
