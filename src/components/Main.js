38c2c9aed115976f1bac8e0cc5322cef9f91e6d6
tbui <nhattuan.dkc@gmail.com>
2022-06-10 19:09:04 +0700

    upload StaffDetail

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,7 @@
 import React, {useState} from "react";
 import Header from "./Header";
 import StaffList from "./StaffList";
+import StaffDetail from "./StaffDetail";
 import Footer from "./Footer";
 import { Switch, Route } from 'react-router-dom';
 import { DEPARTMENTS, STAFFS } from '../shared/staffs';
@@ -10,13 +11,32 @@ function Main() {
         staffs: STAFFS,
         departments: DEPARTMENTS
     });
+
+    const StaffWithId = ({ match }) => {
+        return (
+            <StaffDetail
+                nv={
+                    nhanvien.staffs.filter
+                        ((item) => item.id === parseInt(match.params.nhanvienId, 10)
+                    )[0]
+                }
+            />
+        )
+    }
     return (
         <div>
             <Header />
-            <Route
-                path='/nhanvien'
-                component={() => <StaffList staffs={nhanvien.staffs} />}
-            />
+            <Switch>
+                <Route
+                    exact
+                    path='/nhanvien'
+                    component={() => <StaffList staffs={nhanvien.staffs} />}
+                />
+                <Route
+                    path='/nhanvien/:nhanvienId'
+                    component={StaffWithId}
+                />
+            </Switch>
             <Footer />
         </div>
     );
---
+++ src/components/StaffDetail.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function StaffDetail(props) {
+    console.log(props)
+    return (
+        <div></div>
+    )
+}
+
+export default StaffDetail;
