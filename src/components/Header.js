7bd690988384dc49a7eee8c60e5a5f875b07892a
tbui <nhattuan.dkc@gmail.com>
2022-06-10 20:22:03 +0700

    edit Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,56 @@
-import React from "react";
-import { Navbar, Nav, NavbarBrand, NavItem } from 'reactstrap';
+import React, {Component} from "react";
+import { Navbar, Nav, NavbarBrand, NavItem, NavbarToggler, Collapse } from 'reactstrap';
+import { NavLink } from 'react-router-dom';
 
-function Header() {
-    return (
-        <div>
-            <Navbar color='primary'>
-                <div className="container">
-                    <NavbarBrand>
-                        <img src="assets/images/logo.png" width='40' alt="LOGO"></img>
-                    </NavbarBrand>
-                    <Nav>
-                        <NavItem><span className="fa fa-users fa-lg"></span> Nhan Vien </NavItem>
-                        <NavItem><span className="fa fa-users fa-lg"></span> Phong Ban </NavItem>
-                        <NavItem><span className="fa fa-users fa-lg"></span> Bang Luong </NavItem>
-                    </Nav>
-                </div>
-                
-           </Navbar>
-        </div>
-    );
+class Header extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            isNavOpen: false
+        };
+        this.toggleNav = this.toggleNav.bind(this);
+    }
+
+    toggleNav() {
+        this.setState({
+            isNavOpen: !this.state.isNavOpen
+        });
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                <Navbar dark expand='md' color='primary'>
+                    <div className="container">
+                        <NavbarToggler onClick={this.toggleNav} />
+                        <NavbarBrand className='mr-auto' href="/">
+                            <img src='assets/images/logo.png' height='30' width='41' alt='Ristorante Con Fusion' />
+                        </NavbarBrand>
+                        <Collapse isOpen={this.state.isNavOpen} navbar>
+                            <Nav navbar>
+                                <NavItem>
+                                    <NavLink className='nav-link' to='/nhanvien'>
+                                        <span className='fa fa-users fa-lg'></span> Nhan Vien
+                                    </NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink className='nav-link' to='/phongban'>
+                                        <span className='fa fa-info fa-lg'></span> Phong Ban
+                                    </NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink className='nav-link' to='/bangluong'>
+                                        <span className='fa fa-list fa-lg'></span> Bang Luong
+                                    </NavLink>
+                                </NavItem>
+                            </Nav>
+                        </Collapse>
+                    </div>
+                </Navbar>
+            </React.Fragment>
+        );
+    }
 }
 
 export default Header;
